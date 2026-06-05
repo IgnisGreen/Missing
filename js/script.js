ed069a93516015093a5d707bfbee8b6be6047a78
S <13777719+IgnisGreen@users.noreply.github.com>
2026-06-05 17:20:28 +0200

    wait added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
-  $(document).ready(function() {
 
-    console.log("--- jQuery ready ---")
+$(document).ready(function() {
 
-    
-    $("#form").contents().find("body").html("<div> blah </div>");
+    console.log('--- jQuery ready ---');
+
+    console.log($('#form').contents().length);
+    console.log($('#form').contents());
+
+
+
+
 });
+
+    //$('#form').contents().find('body').html('<div> blah </div>');
+
+
+    function waitUntilConditionIsMet() {
+  console.log($('#form').contents().length);
+
+    if ($('#form').contents().length <= 1) {
+        setTimeout(
+          //waitUntilConditionIsMet(), 
+          50000
+        );
+    } else {
+        // add logic for adding jQuery Library
+    }
+};
+//waitUntilConditionIsMet();
